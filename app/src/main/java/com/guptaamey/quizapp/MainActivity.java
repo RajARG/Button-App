f577bf6e47c731df59d0ceb2fd7733a95b259de9
1707592 <gupta.amey@gmail.com>
2020-02-03 15:59:00 -0500

    Day 2

--- app/src/main/java/com/guptaamey/quizapp/MainActivity.java
+++ app/src/main/java/com/guptaamey/quizapp/MainActivity.java
@@ -1,14 +1,25 @@
 package com.guptaamey.quizapp;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submitButton = findViewById(R.id.clickButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Test Button","Hello Mom.");
+            }
+        });
     }
 }
