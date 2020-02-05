8266f07bbdc271e0ffc28de0f6277b5ecba68af1
1707592 <gupta.amey@gmail.com>
2020-02-05 15:58:25 -0500

    2/5/2020

--- app/src/main/java/com/guptaamey/quizapp/MainActivity.java
+++ app/src/main/java/com/guptaamey/quizapp/MainActivity.java
@@ -4,10 +4,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.graphics.Color;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button submitButton;
+    Button colorButton;
+    EditText nameText;
+    TextView displayText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +22,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         submitButton = findViewById(R.id.clickButton);
+        colorButton = findViewById(R.id.clickButton2);
+        nameText = findViewById(R.id.responseEditText);
+        colorText = findViewById(R.id.response2EditText);
+        displayText = findViewById(R.id.textBox);
+
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("Test Button","Hello Mom.");
+                Log.i("Test Button1", nameText.getText().toString());
+                String s = displayText.getText()+" "+ nameText.getText();
+                displayText.setText(s);
             }
         });
+
+        colorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Test Button2", nameText.getText().toString());
+                String colorVal = colorText.get
+                colorButton.setTextColor(Color.parseColor(Color.blue));
+            }
+        });
+
+        nameText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus){
+                    if(nameText.getText().toString().equals("TJ")){
+                        displayText.setText("TJ Rocks!");
+                        nameText.setText("");
+                        nameText.setHint("That's a good name.");
+                    }
+                }
+            }
+        });
+
     }
 }
